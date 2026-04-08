8635d789d84a085987b9a517b2c539726fef62ad
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2026-04-08 20:12:37 +0530

    Update and rename Dockerfile to backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backup-s3-kamble "   # Replace with your S3 bucket name
+    key            = "terraform.tfstate"       # Path inside the bucket
+    region         = "ap-south-1"                  # Mumbai region (change if needed)
+    encrypt        = true
+  }
+}
